5fd3faf782c10b0a7e5a551c0ace10a9bbfdd595
Nifahs <shafinmuhammad28@gmail.com>
2020-08-24 03:38:34 +0600

    truthand false

---
+++ truthy_falsy.js
@@ -0,0 +1,51 @@
+const name = 'a';
+const name1 = '';
+const num = 0;
+const num1 = 1;
+
+const name2 = NaN;
+const name3 = null;
+const age =3;
+
+
+if(name){
+    console.log('true');
+}
+else{
+    console.log('false')
+}
+
+if(name1){
+    console.log('true');
+}
+else{
+    console.log('false')
+}
+
+if(name2){
+    console.log('true');
+}
+else{
+    console.log('false')
+}
+
+if(name3){
+    console.log('true');
+}
+else{
+    console.log('false')
+}
+
+if(num){
+    console.log('true');
+}
+else{
+    console.log('false')
+}
+
+if(num1){
+    console.log('true');
+}
+else{
+    console.log('false')
+}
